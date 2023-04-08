85d0e1c3ba055b4a047041b57dc06f9f1938bebc
Eric Le <67171800+Smickeroni@users.noreply.github.com>
2023-04-08 11:37:52 -0700

    Update clippy.js
    browser command + clipping functionality?

--- clippy/clippy.js
+++ clippy/clippy.js
@@ -1,3 +1,16 @@
 browser.windows.getCurrent({ populate: true }).then((windowInfo) => {
     myWindowId = windowInfo.id;
   });
+
+  browser.commands.onCommand.addListener((command) => {
+    if (command === "_add_text_to_clippy") {
+      clip()
+      .then(console.log("Command is running 👍"))
+    }
+  });
+
+  //Clipping functionality
+const clip = () => {
+  navigator.clipboard.readText()
+  .then( (clippedText) => {console.log("is this what you currently have clipped?:" + clippedText)} );
+}
